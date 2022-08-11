5a518cc3e897ad7d10758e034cd9d76dac107107
Abduqodir077 <urinovabduqodir07@gmail.com>
2022-08-12 00:09:05 +0500

    quote generate

---
+++ js.js
@@ -0,0 +1,16 @@
+function generate(){
+    let quotes = {
+        "- Jules Renard" : '"The truly free man is the one who can turn down an invitation to dinner without giving an excuse."',
+        "- Albert Einstein" : '"Our task must be to free ourselves... by widening our circle of compassion to embrace all living creatures and the whole of nature and its beauty."',
+        "- Pierce Brown" : '"Man cannot be freed by the same injustice that enslaved it."'
+    }
+
+    let authors = Object.keys(quotes);
+
+    let author = authors[Math.floor(Math.random() * authors.length)];
+
+    let quote = quotes[author];
+
+    document.getElementById("quote").innerHTML = quote;
+    document.getElementById("author").innerHTML = author;
+}
