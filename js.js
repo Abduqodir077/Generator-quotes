50d1c0d955bc82a6d8795dfe4cac6872640beaeb
Abduqodir077 <urinovabduqodir07@gmail.com>
2022-08-17 22:08:52 +0500

    add new quotes

--- js.js
+++ js.js
@@ -1,9 +1,20 @@
 function generate(){
     let quotes = {
-        "- Jules Renard" : '"The truly free man is the one who can turn down an invitation to dinner without giving an excuse."',
-        "- Albert Einstein" : '"Our task must be to free ourselves... by widening our circle of compassion to embrace all living creatures and the whole of nature and its beauty."',
-        "- Pierce Brown" : '"Man cannot be freed by the same injustice that enslaved it."'
+        "- Наполеон Бонапарт" : '"Дай человеку власть, и ты узнаешь, кто он."',
+        "- Марк Твен" : '"Никогда не спорьте с идиотами. Вы опуститесь до их уровня, где они задавять своим опытом"',
+        "- ??? " : '"Если вы общаетесь с четыремя милионерами вы будете пятым, если вы общаетесь с тремя уверенными в себя людьми вы будете четвёртым, если вы общаетесь с двумя идиотами вы будете третим."'
     }
+
+    // const data = [
+    //     {
+    //         id: 0,
+    //         img: "./img/1519855918965.jfif"    
+    //     },
+    //     {
+    //         id: 1,
+    //         img: "./img/1575479070098.jfif"
+    //     }
+    // ]
 
     let authors = Object.keys(quotes);
 
